74242a39e91591c29eb53ad11d49ffde808446c4
mwild <mwild@43a219fd-4765-4d09-99ce-3c4172bfe293>
2011-10-20 04:51:31 +0000

    ENH: Added mcColdReactionModel class
    Signed-off-by: Michael Wild <mwild@ifd.mavt.ethz.ch>
    
    git-svn-id: https://svn.origo.ethz.ch/pdffoam/trunk@81 43a219fd-4765-4d09-99ce-3c4172bfe293

---
+++ mcParticle/mcReactionModel/mcColdReactionModel/mcColdReactionModel.C
@@ -0,0 +1,75 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 1991-2010 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "mcColdReactionModel.H"
+
+#include "addToRunTimeSelectionTable.H"
+#include "mcParticleCloud.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+    defineTypeNameAndDebug(mcColdReactionModel, 0);
+    addNamedToRunTimeSelectionTable
+    (
+        mcReactionModel,
+        mcColdReactionModel,
+        mcReactionModel,
+        Cold
+    );
+
+} // namespace Foam
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::mcColdReactionModel::mcColdReactionModel(const Foam::dictionary& dict)
+:
+    mcReactionModel(dict),
+    rho_(dict.lookupOrDefault<scalar>("density", 1.0))
+{}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::mcColdReactionModel::correct(Foam::mcParticleCloud& cloud)
+{
+    forAllIter(mcParticleCloud, cloud, pIter)
+    {
+        correct(cloud, pIter());
+    }
+}
+
+
+void Foam::mcColdReactionModel::correct
+(
+    Foam::mcParticleCloud& cloud,
+    Foam::mcParticle& p
+)
+{
+    p.rho() = rho_;
+}
+
+// ************************************************************************* //
