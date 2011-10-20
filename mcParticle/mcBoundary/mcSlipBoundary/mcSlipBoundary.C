b84cce028412f10ef8f8241ae4eb8e23be4c2cf1
mwild <mwild@43a219fd-4765-4d09-99ce-3c4172bfe293>
2011-10-20 13:22:45 +0000

    ENH: Added slip-wall boundary class mcSlipBoundary
    Signed-off-by: Michael Wild <mwild@ifd.mavt.ethz.ch>
    
    git-svn-id: https://svn.origo.ethz.ch/pdffoam/trunk@138 43a219fd-4765-4d09-99ce-3c4172bfe293

---
+++ mcParticle/mcBoundary/mcSlipBoundary/mcSlipBoundary.C
@@ -0,0 +1,80 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 1991-2010 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "mcSlipBoundary.H"
+
+#include "addToRunTimeSelectionTable.H"
+#include "mcParticle.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+    defineTypeNameAndDebug(mcSlipBoundary, 0);
+    addNamedToRunTimeSelectionTable
+    (
+        mcBoundary,
+        mcSlipBoundary,
+        mcBoundary,
+        slip
+    );
+
+} // namespace Foam
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::mcSlipBoundary::mcSlipBoundary
+(
+    const Foam::fvMesh& mesh,
+    Foam::label patchID,
+    const Foam::dictionary& dict
+)
+:
+    mcBoundary(mesh, patchID, dict)
+{}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::mcSlipBoundary::hitPatch
+(
+    Foam::mcParticle& p,
+    Foam::mcParticle::trackData& td
+)
+{
+    vector nw = patch().faceAreas()[patch().whichFace(p.face())];
+    nw /= mag(nw);  // Wall normal (outward)
+    p.transformProperties(I - 2.0*nw*nw);
+}
+
+
+void Foam::mcSlipBoundary::hitPatch
+(
+    Foam::mcParticle& p,
+    int&
+)
+{}
+
+// ************************************************************************* //
